267a03716eddd35d137e494cdc55b3ae9f680258
clint <cpsandbox@gmail.com>
2016-12-19 11:36:39 -0800

    Dec 19 merge

--- Team9374/src/main/java/org/firstinspires/ftc/team9374/Distance_Function_TankD.java
+++ Team9374/src/main/java/org/firstinspires/ftc/team9374/Distance_Function_TankD.java
@@ -1,38 +1,43 @@
 package org.firstinspires.ftc.team9374;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+/*
+=======
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 /**
+>>>>>>> e920dc6351b8ece876e59ccd20e250de48f454da
  * Created by darwin on 9/20/16.
  */
 
-@TeleOp(name="Basic Tank Drive!", group="Normal_Opmode")
+@TeleOp(name="Basic Tank Drive!", group="Noramal_Opmode")
+@Disabled
 
 public class Distance_Function_TankD extends OpMode {
-    DcMotor leftFront;
-    DcMotor rightFront;
-    DcMotor leftBack;
-    DcMotor rightBack;
-    MotorMatr Test = new MotorMatr();
+    DcMotor left;
+    DcMotor right;
+
     public void init()  {
-        leftFront = hardwareMap.dcMotor.get("Motor-left");
-        rightFront = hardwareMap.dcMotor.get("Motor-right");
-        leftBack = hardwareMap.dcMotor.get("Motor-rear-left");
-        rightBack = hardwareMap.dcMotor.get("Motor-rear-right");
-        // Combine all the motors into a single Object; [[0, 1, 2, 3], ...]
-        Test.addMotors(0, leftFront, rightFront);
-        Test.addMotors(1, leftBack, rightBack);
+        left = hardwareMap.dcMotor.get("Motor-left");
+        right = hardwareMap.dcMotor.get("Motor-right");
     }
 
-    public void loop() {
-        float leftDC = -gamepad1.left_stick_y;
-        float rightDC =  -gamepad1.right_stick_y;
 
-        // Run all motors at [0]
-        Test.runMotors(0, leftDC);
-        // Run all motors at [1]
-        Test.runMotors(1, rightDC);
+    @Override
+    public void loop() {
+        float leftDC = gamepad1.left_stick_y;
+        float rightDC =  gamepad1.right_stick_y;
+
+
+        left.setPower(leftDC);
+        right.setPower(rightDC);
+        //if(leftDCy > 0)
+
+
+
     }
 }
